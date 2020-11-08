812d805fe910138e2ed6b0f9230cfce153dce22c
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-11-08 16:54:36 +0100

    css, webpack, eslint, prettier boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '~/styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
