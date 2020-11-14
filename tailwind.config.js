ea91e43c96187acd89b0a7b5ebb17d80c741bdf8
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-11-14 19:40:13 +0100

    add tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {},
   },
