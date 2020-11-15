07c26cf7b0610e706be3c21ca984c7a798faef15
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-11-15 20:05:19 +0100

    disable page index, lint file

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-import Head from 'next/head'
-import '~/styles/index.css';
+import Head from "next/head";
+import "~/styles/index.css";
 import Header from "~/components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <meta name="robots" content="noindex" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
       <Header />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
